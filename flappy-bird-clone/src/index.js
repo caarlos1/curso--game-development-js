db41475095587f8bb86983301873d25ae016a2d2
Carlos Roberto <crpu4a@gmail.com>
2022-06-24 00:04:20 -0300

    Progress of curse until lesson 25

--- flappy-bird-clone/src/index.js
+++ flappy-bird-clone/src/index.js
@@ -9,9 +9,9 @@ const config = {
         default: "arcade",
         arcade: {
             debug: true,
-            gravity: {
-                y: 400,
-            },
+            // gravity: {
+            //     y: 400,
+            // },
         },
     },
     scene: {
@@ -21,16 +21,24 @@ const config = {
     },
 };
 
+const PIPES_TO_RENDER = 4;
+
 let bird = null;
+let pipes = null;
+
+const pipeVerticalDistanceRange = [150, 250];
+const pipeHorizontalDistanceRange = [300, 400];
 
 const flapVelocity = 250;
 const initialBirdPosition = {
     x: config.width * 0.1,
     y: config.height / 2,
 };
+
 function preload() {
     this.load.image("sky", "assets/sky.png");
     this.load.image("bird", "assets/bird.png");
+    this.load.image("pipe", "assets/pipe.png");
 }
 
 function create() {
@@ -42,16 +50,26 @@ function create() {
         .sprite(initialBirdPosition.x, initialBirdPosition.y, "bird")
         .setOrigin(0);
 
+    bird.body.gravity.y = 400;
+
+    pipes = this.physics.add.group();
+
+    for (let i = 0; i < PIPES_TO_RENDER; i++) {
+        const upperPipe = pipes.create(0, 0, "pipe").setOrigin(0, 1);
+        const lowerPipe = pipes.create(0, 0, "pipe").setOrigin(0, 0);
+        placePipe(upperPipe, lowerPipe);
+    }
+
+    pipes.setVelocityX(-250);
+
     this.input.on("pointerdown", flap);
     this.input.keyboard.on("keydown_SPACE", flap);
 }
 
-// 60 times per second
-// update(time, delta)
 function update() {
-    if (bird.y > config.height || bird.y < -bird.height) {
-        restarPlayerPosition();
-    }
+    if (bird.y > config.height || bird.y < -bird.height) restarPlayerPosition();
+
+    recyclePipes();
 }
 
 function restarPlayerPosition() {
@@ -59,8 +77,53 @@ function restarPlayerPosition() {
     bird.y = initialBirdPosition.y;
     bird.body.velocity.y = -flapVelocity / 4;
 }
+
 function flap() {
     bird.body.velocity.y = -flapVelocity;
+}
+
+function getRightMostPipe() {
+    let rightMostX = 0;
+
+    pipes.getChildren().forEach(function (pipe) {
+        rightMostX = Math.max(pipe.x, rightMostX);
+    });
+
+    return rightMostX;
+}
+
+function placePipe(uPipe, lPipe) {
+    const rightMostX = getRightMostPipe();
+
+    const pipeVerticalDistance = Phaser.Math.Between(
+        ...pipeVerticalDistanceRange
+    );
+    const pipeVerticalPosition = Phaser.Math.Between(
+        0 + 20,
+        config.height - 20 - pipeVerticalDistance
+    );
+
+    const pipeHorizontalDistance = Phaser.Math.Between(
+        ...pipeHorizontalDistanceRange
+    );
+
+    uPipe.x = rightMostX + pipeHorizontalDistance;
+    uPipe.y = pipeVerticalPosition;
+
+    lPipe.x = uPipe.x;
+    lPipe.y = uPipe.y + pipeVerticalDistance;
+}
+
+function recyclePipes() {
+    const tempPipes = [];
+    pipes.getChildren().forEach((pipe) => {
+        if (pipe.getBounds().right <= 0) {
+            tempPipes.push(pipe);
+            if (tempPipes.length == 2) {
+                placePipe(...tempPipes);
+            }
+        }
+    });
 }
 
 new Phaser.Game(config);
